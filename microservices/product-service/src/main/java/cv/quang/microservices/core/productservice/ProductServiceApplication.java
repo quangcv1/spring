b2625b386f75d4d71931b2392badc8c2dad85884
DESKTOP-B09ANSL\Quang <quangcv@icloud.com>
2020-11-11 11:49:20 +0700

    First sekeleton for 4 services

---
+++ microservices/product-service/src/main/java/cv/quang/microservices/core/productservice/ProductServiceApplication.java
@@ -0,0 +1,13 @@
+package cv.quang.microservices.core.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+
+}
